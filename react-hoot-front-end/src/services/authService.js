471eb32ce9b76e17ab3fdcf61750fd45acdc2235
jordanjc-ShirtHanger <devvcarter4ever24@outlook.com>
2024-11-15 09:35:07 -0600

    Signed up user in backend

--- react-hoot-front-end/src/services/authService.js
+++ react-hoot-front-end/src/services/authService.js
@@ -1,54 +1,67 @@
-const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL;
+// src/services/authService.js
 
-const getUser = () => {
-  const token = localStorage.getItem('token');
-  if (!token) return null;
-  const user = JSON.parse(atob(token.split('.')[1]));
-  return user;
-};
+import axios from "axios"
 
-const signup = async (formData) => {
-  try {
-    const res = await fetch(`${BACKEND_URL}/users/signup`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(formData),
-    });
-    const json = await res.json();
-    if (json.error) {
-      throw new Error(json.error);
+const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL; // The Express API url
+
+/* Allows new user to sign up */
+async function signup(formData) {
+
+    try { /* Creates user object from the REACT front end */
+        const response = await axios.post(`${BACKEND_URL}/users/signup`, formData)
+
+        if (response.data.error) {
+            throw new Error(response.data.error)
+        }
+
+        if (response.data.token) {
+            localStorage.setItem('token', response.data.token); // Stores the JWT token into the browser's localStorage
+
+            const user = JSON.parse(atob(response.data.token.split('.')[1]))
+            return user;
+        }
+
+    } catch (error) {
+        console.log(error)
     }
-    localStorage.setItem('token', json.token);
-    return json;
-  } catch (err) {
-    throw new Error(err);
-  }
-};
+}
 
-const signin = async (user) => {
-  try {
-    const res = await fetch(`${BACKEND_URL}/users/signin`, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(user),
-    });
-    const json = await res.json();
-    if (json.error) {
-      throw new Error(json.error);
+/* Allows existing user to sign in */
+async function signin(user) {
+
+    try { /* Accesses exsiting user object from the REACT front end */
+        const response = await axios.post(`${BACKEND_URL}/users/signin`, user)
+
+        if (response.data.error) {
+            throw new Error(response.data.error)
+        }
+
+        /* Takes "Bearer" out of the token response */
+        if (response.data.token) {
+            localStorage.setItem('token', response.data.token); // Stores the JWT token into the browser's localStorage
+
+            const user = JSON.parse(atob(response.data.token.split('.')[1]))
+            return user;
+        }
+
+        return response.data
+
+    } catch (error) {
+        console.log(error)
     }
-    if (json.token) {
-      localStorage.setItem('token', json.token);
-      const user = JSON.parse(atob(json.token.split('.')[1]));
-      return user;
-    }
-  } catch (err) {
-    console.log(err);
-    throw err;
-  }
-};
+}
 
-const signout = () => {
-  localStorage.removeItem('token');
-};
+/* Will check if the user has signed in on a previous session, if so, loads their profile no problem. If not, sets to null */
+function getUser() {
 
-export { signup, signin, getUser, signout };
+    const token = localStorage.getItem('token');
+    if (!token) return null;
+    const user = JSON.parse(atob(token.split('.')[1]));
+    return user;
+}
+
+function signout() {
+    localStorage.removeItem('token');
+}
+
+export { signup, signin, getUser, signout }
